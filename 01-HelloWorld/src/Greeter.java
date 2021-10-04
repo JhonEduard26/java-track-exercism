c78dfe35ca42ef778b8ead21755563d42681e879
Jhon Bocanegra <jhon.bocanegra26@gmail.com>
2021-10-03 20:22:38 -0500

    Finalizado HelloWorld

---
+++ 01-HelloWorld/src/Greeter.java
@@ -0,0 +1,9 @@
+public class Greeter {
+    static String getGreetings(){
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreetings());
+    }
+}
